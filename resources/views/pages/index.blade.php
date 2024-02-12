b651709d67a6b388f88cbcb5630e888083b75231
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-12 17:15:14 +0100

    add: content

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.main-layout')
 @section('head')
-    <title>Home</title>
+<title>Home</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+<h1>Movies</h1>
+<ul>
+    @foreach ($movies as $movie)
+
+    <li>
+        {{ $movie -> title }}
+    </li>
+    @endforeach
+</ul>
 @endsection
