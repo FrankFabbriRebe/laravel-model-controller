dd7071afbb50f189e9f5370c61af38a2ec7a098a
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-12 17:15:00 +0100

    delete: header and footer

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,11 +10,11 @@
 
     @yield('head')
 </head>
+
 <body>
     <div class="text-center">
-        @include('components.header')
         @yield('content')
-        @include('components.footer')
     </div>
 </body>
+
 </html>
